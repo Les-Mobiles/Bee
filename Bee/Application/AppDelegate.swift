df3a25dd77dc512de3361ddac8b71b7fbf325ba3
Santiago Avila <sanarroya@gmail.com>
2020-11-02 09:09:12 -0500

    Added MenuBar Item

--- Bee/Application/AppDelegate.swift
+++ Bee/Application/AppDelegate.swift
@@ -12,28 +12,14 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
-
+    var menuBarConfigurator: MenuBarHandler!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView()
-
-        // Create the window and set the content view.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.isReleasedWhenClosed = false
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        menuBarConfigurator = MenuBarHandler()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
---
+++ Bee/Application/MenuBar/MenuBarHandler.swift
@@ -0,0 +1,85 @@
+//
+//  MenuBarConfigurator.swift
+//  Bee
+//
+//  Created by Santiago Avila Arroyave on 11/1/20.
+//
+
+import SwiftUI
+
+class MenuBarHandler: NSObject {
+    
+    var statusItem: NSStatusItem!
+    
+    override init() {
+        super.init()
+        buildStatusItem()
+    }
+}
+
+private extension MenuBarHandler {
+    
+    enum MenuItem: CaseIterable {
+        case preferences
+        case quit
+        
+        var title: String {
+            switch self {
+                case .preferences:
+                    return "Preferences..."
+                case .quit:
+                    return "Quit"
+            }
+        }
+        
+        var action: Selector {
+            switch self {
+                case .preferences:
+                    return #selector(presentSettings)
+                case .quit:
+                    return #selector(NSApplication.terminate(_:))
+            }
+        }
+        
+    }
+    
+    @objc func presentSettings() {
+        let contentView = ContentView()
+        let window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        
+        guard let appDelegate = NSApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        appDelegate.window = window
+        appDelegate.window.isReleasedWhenClosed = false
+        appDelegate.window.center()
+        appDelegate.window.setFrameAutosaveName("Main Window")
+        appDelegate.window.contentView = NSHostingView(rootView: contentView)
+        appDelegate.window.makeKeyAndOrderFront(nil)
+    }
+    
+    func buildStatusItem() {
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        if let button = statusItem.button {
+            button.image = NSImage(named: NSImage.Name("MenuIcon"))
+        }
+        statusItem.menu = menu()
+    }
+    
+    func menu() -> NSMenu {
+        let menu = NSMenu()
+        MenuItem.allCases.forEach {
+            let item = NSMenuItem(title: $0.title, action: $0.action, keyEquivalent: "")
+            if $0 == .preferences {
+                item.target = self
+            }
+            menu.addItem(item)
+            menu.addItem(NSMenuItem.separator())
+        }
+        return menu
+    }
+}
