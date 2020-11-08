fb155f327c4f30b5d7c3347cb0e6e17d07188c54
Santiago Avila <sanarroya@gmail.com>
2020-11-08 11:11:26 -0500

    Added MenuBar Item

---
+++ BeeTests/Application/Mocks/AppDelegateMock.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegateMock.swift
+//  BeeTests
+//
+//  Created by Santiago Avila Arroyave on 11/8/20.
+//
+
+import Cocoa
+@testable import Bee
+
+class AppDelegateMock: NSObject, AppDelegatable {
+
+    var window: NSWindow!
+  
+}
